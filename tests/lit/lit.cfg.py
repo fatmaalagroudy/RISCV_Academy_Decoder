6a6fa9236f8db9966b0bbe534bfe954eae2b0cc5
fatmaalagroudy <fatma.alagroudy@gmail.com>
2026-09-22 16:46:06 +0300

    fix lit.cfg

--- tests/lit/lit.cfg.py
+++ tests/lit/lit.cfg.py
@@ -2,7 +2,7 @@ import lit.formats
 import os
 
 config.name = "Decoder"
-config.test_format = lit.formats.ShTest(True)
+config.test_format = lit.formats.ShTest(False)
 config.suffixes = [".test"]
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.my_obj_root, "tests", "lit") \
